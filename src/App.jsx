aa6dfa69b23f25c37b10ea450c037653782b522e
Kaye <kayearroyo42@gmail.com>
2025-03-25 22:09:45 +0800

    Second Course.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import React from 'react'
+import Header from './sections/Header.jsx'
 
 const App = () => {
   return (
-    <div>
-     <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </div>
+   <main className="overflow-hidden">
+    <Header />
+    </main> 
   )
 }
 
